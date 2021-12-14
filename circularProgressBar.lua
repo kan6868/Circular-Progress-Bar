2006575a89b6a32e244113bc14c07ce9c6a26d4e
hqkhanhdev <70838508+hqkhanhdev@users.noreply.github.com>
2021-12-14 11:37:26 +0700

    update

--- circularProgressBar.lua
+++ circularProgressBar.lua
@@ -9,12 +9,15 @@ function M.new(options)
     local scale         = options.scale or 1
     local x             = options.x or display.contentCenterX
     local y             = options.y or display.contentCenterY
+    local font          = options.font
+    local color         = options.color or {0, 0, 0}
     local timeCount     = options.time or 5000
     local onComplete    = options.onComplete or function() end
     
     local group = display.newGroup()
     parent:insert(group)
     group.x, group.y = x, y
+    group.value = 0
     group:scale(scale, scale)
     local bg = display.newImageRect(group, dir.. "bg" .. ext, 128, 128)
 
@@ -28,10 +31,33 @@ function M.new(options)
     local cover = display.newImageRect(group, dir .. "cover_content" .. ext, 128, 128)
     
     -- local fg = display.newImageRect(group, dir .. "fg" .. ext, 512, 512)
-    
+
     local center = display.newImageRect(group, dir .. "center" .. ext, 128, 128)
-    
+
+    local lbl = display.newText(group, group.value .. "%", 0, 0, font, 30)
+    lbl:setFillColor(unpack(color))
+    lbl.alpha = 0
     -- local icon = display.newImageRect(group, dir .. "icon" .. ext, 512, 512) -- icon
+
+    function group:setValue(value, onComplete)
+        if not self.isProgressBar then return false end
+        local onComplete = onComplete or function() end
+        local value = math.floor(value)
+        if value < 0 or value > 100 then
+            return false
+        end
+        local plus = 3.6 * value
+        if value <= 50 then
+            contentLeft.alpha = 0
+            cover.alpha = 1
+            contentRight.rotation = -180 + plus
+        else
+            cover.alpha = 0
+            contentLeft.alpha = 1
+            contentLeft.rotation = 0 + plus
+        end
+        lbl.text = value .. "%"
+    end
 
     function group:count(time, onComplete)
         group:show()
@@ -61,6 +87,19 @@ function M.new(options)
         self.x, self.y = x, y
     end
 
+    function group:finalize()
+
+        Runtime:removeEventListener("enterFrame", enterFrame)
+    end
+    local function enterFrame()
+        if group.isProgressBar then
+            lbl.alpha = 1
+        else
+            lbl.alpha = 0
+        end
+    end
+    group:addEventListener("finalize")
+    Runtime:addEventListener("enterFrame", enterFrame)
     return group
 end
 
--- main.lua
+++ main.lua
@@ -1,6 +1,22 @@
 local CircularProgressBar = require "circularProgressBar"
 
 local progressBar = CircularProgressBar.new({scale = 2})
-progressBar:count(5000, function ()
-    print("CircularProgressBar Completed.")
+progressBar:count(5000, function()
+    print("CircularProgressBar Done.")
 end)
+--[[
+    working as progressbar
+    local value = 1
+    local dir = 1
+    progressBar.isProgressBar = true
+    timer.performWithDelay(25, function()
+        if value > 100 then
+            dir = -1
+        end
+        value = value + 1 * dir
+
+        progressBar:setValue(value)
+    
+    end, 202)
+]]
+
